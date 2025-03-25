4526e35b45e4d2b1968849faa3517b9d417a467c
Michael John Čverčko <michaelcvercko123@gmail.com>
2025-03-25 14:45:31 +0100

    Ambulance waiting list CRUD operations

--- src/components/mjc-ambulance-wl-app/mjc-ambulance-wl-app.tsx
+++ src/components/mjc-ambulance-wl-app/mjc-ambulance-wl-app.tsx
@@ -56,6 +56,7 @@ export class MjcAmbulanceWlApp {
       <Host>
       { element === "editor"
       ? <mjc-ambulance-wl-editor entry-id={entryId}
+        ambulance-id={this.ambulanceId} api-base={this.apiBase}
         oneditor-closed={ () => navigate("./list")} >
       </mjc-ambulance-wl-editor>
       : <mjc-ambulance-wl-list  ambulance-id={this.ambulanceId} api-base={this.apiBase}
